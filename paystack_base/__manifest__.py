21e9b2d01098ee489bbfbb93d3ec828a9c4694c8
Zadock Maloba <malobazadock@gmail.com>
2025-01-05 10:38:32 +0300

    Resolve installation issues in paystack_base

--- paystack_base/__manifest__.py
+++ paystack_base/__manifest__.py
@@ -1,13 +1,13 @@
 {
     'name': 'Paystack Payment Gateway',
-    'version': '14.0.1.0.0',
+    'version': '17.0.1.0.0',
     'category': 'Website',
-    'summary': 'Paystack Payment Gateway Integration for Odoo 14',
-    'description': 'Paystack Payment Gateway Integration for Odoo 14, Paystack, payment gateway,Payment Gateway Integration,Paystack payment, odoo 14, odoo payment gateway',
+    'summary': 'Paystack Payment Gateway Integration for Odoo 17',
+    'description': 'Paystack Payment Gateway Integration for Odoo 17, Paystack, payment gateway,Payment Gateway Integration,Paystack payment, odoo 17, odoo payment gateway',
     'author': 'Paystack',
     'company': 'Paystack',
     'website': 'https://paystack.com',
-    'depends': ['base','payment'],
+    'depends': ['base', 'payment'],
     'data': [
         'views/payment_acquirers_views.xml',
         'views/templates.xml',
--- paystack_base/models/payment_acquirers.py
+++ paystack_base/models/payment_acquirers.py
@@ -1,59 +1,71 @@
-from odoo.exceptions import UserError, Warning
-from odoo import api, fields, models
+from odoo.exceptions import UserError, warnings
+from odoo import fields, models
 import requests
 from odoo.http import request
 import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class PaymentAcquirerPaystack(models.Model):
-    _inherit = 'payment.acquirer'
+    _inherit = 'payment.provider'
 
-    provider = fields.Selection(selection_add=[('paystack', 'Paystack')], ondelete={'paystack': 'set default'})
+    provider = fields.Selection([('paystack', 'Paystack')], ondelete={'paystack': 'set default'})
 
-    paystack_transaction_initialize = fields.Char(string="Transaction Initialize", default="https://api.paystack.co/transaction/initialize", required_if_provider="paystack", groups="base.group_user", help="Used to initiate payment for the customer")
+    # provider = fields.Selection([
+    #     ('paypal', 'PayPal'),
+    #     ('stripe', 'Stripe'),
+    #     ('manual', 'Manual Payment'),
+    #     ('paystack', 'Paystack'),
+    # ], string="Provider", ondelete={'paystack': 'set default'})
+    #
 
-    paystack_transaction_verify = fields.Char(string="Transaction Verify", default="https://api.paystack.co/transaction/verify", required_if_provider="paystack", groups="base.group_user", help="verify/validate the payment details after the gateway response")
+    paystack_transaction_initialize = fields.Char(string="Transaction Initialize", default="https://api.paystack.co/transaction/initialize",
+                                                  required_if_provider="paystack", groups="base.group_user", help="Used to initiate payment for the customer")
 
-    paystack_transaction_refund = fields.Char(string="Transaction Refund", default="https://api.paystack.co/refund", required_if_provider="paystack", groups="base.group_user",help="Used to initiate refund to customer")
+    paystack_transaction_verify = fields.Char(string="Transaction Verify", default="https://api.paystack.co/transaction/verify",
+                                              required_if_provider="paystack", groups="base.group_user", help="verify/validate the payment details after the gateway response")
 
-    paystack_merchant_key = fields.Char("Secret Key", required_if_provider="paystack", groups="base.group_user")
+    paystack_transaction_refund = fields.Char(string="Transaction Refund", default="https://api.paystack.co/refund",
+                                              required_if_provider="paystack", groups="base.group_user", help="Used to initiate refund to customer")
 
-    ecom_allowed_currencies = fields.Many2many("res.currency",string="Allowed Currencies")
+    paystack_merchant_key = fields.Char(
+        "Secret Key", required_if_provider="paystack", groups="base.group_user")
+
+    ecom_allowed_currencies = fields.Many2many("res.currency", string="Allowed Currencies")
 
     def test_paystack_initialization(self):
         endpoint = self.paystack_transaction_initialize.rstrip("/")
-        data = { 
-            "email": "test@gmail.com", #mandatory
-            "amount": "1000", #mandatory
-            'currency': "NGN", #optional
-            'callback_url': "https://www.pptservices.com", #optional
-            }
-        bearer_token = "Bearer "+str(self.paystack_merchant_key)
+        data = {
+            "email": "test@gmail.com",  # mandatory
+            "amount": "1000",  # mandatory
+            'currency': "NGN",  # optional
+            'callback_url': "https://www.pptservices.com",  # optional
+        }
+        bearer_token = "Bearer " + str(self.paystack_merchant_key)
         headers = {"Authorization": bearer_token}
 
         api_call = requests.post(endpoint, data=data, headers=headers)
-        if api_call.status_code==200:
-            raise Warning("Payment Credential Seems Perfect")
+        if api_call.status_code == 200:
+            raise warnings.WarningMessage("Payment Credential Seems Perfect")
         else:
             raise UserError("Invalid Initialize URL/Secret Key")
-    
+
     def test_paystack_refund(self):
         data = {
-                'transaction': "test", #mandatory
-                'amount': "500", #optional
-                'currency': "NGN", #optional
-                'customer_note': "Test", #optional
-                'merchant_note': "Test", #optional
-            }
+            'transaction': "test",  # mandatory
+            'amount': "500",  # optional
+            'currency': "NGN",  # optional
+            'customer_note': "Test",  # optional
+            'merchant_note': "Test",  # optional
+        }
 
         endpoint = self.paystack_transaction_refund.rstrip("/")
-        
-        bearer_token = "Bearer "+str(self.paystack_merchant_key)
+        bearer_token = "Bearer " + str(self.paystack_merchant_key)
         headers = {"Authorization": bearer_token}
 
         api_call = requests.post(endpoint, data=data, headers=headers)
-        
+
         try:
             response = api_call.json()
             if response.get("message") == "Transaction not found":
@@ -62,22 +74,21 @@ class PaymentAcquirerPaystack(models.Model):
                 verification = "failed"
         except:
             verification = "failed"
-        
-        if verification=="success":
-            raise Warning("Refund Credential Seems Perfect")
+
+        if verification == "success":
+            warnings.WarningMessage("Refund Credential Seems Perfect")
         else:
             raise UserError("Invalid Refund URL/Secret Key")
-        
 
     def call_paystack_transaction_initialize(self, data):
 
         endpoint = self.paystack_transaction_initialize.rstrip("/")
-        
-        bearer_token = "Bearer "+str(self.paystack_merchant_key)
+
+        bearer_token = "Bearer " + str(self.paystack_merchant_key)
         headers = {"Authorization": bearer_token}
 
         api_call = requests.post(endpoint, data=data, headers=headers)
-        if api_call.status_code==200:
+        if api_call.status_code == 200:
             response = api_call.json()
         else:
             response = {
@@ -86,16 +97,16 @@ class PaymentAcquirerPaystack(models.Model):
             }
 
         return response
-    
+
     def call_paystack_transaction_verify(self, reference):
 
-        endpoint = self.paystack_transaction_verify.rstrip("/")+"/"+str(reference)
-        
-        bearer_token = "Bearer "+str(self.paystack_merchant_key)
+        endpoint = self.paystack_transaction_verify.rstrip("/") + "/" + str(reference)
+
+        bearer_token = "Bearer " + str(self.paystack_merchant_key)
         headers = {"Authorization": bearer_token}
 
         api_call = requests.get(endpoint, headers=headers)
-        if api_call.status_code==200:
+        if api_call.status_code == 200:
             response = api_call.json()
         else:
             response = {
@@ -106,14 +117,14 @@ class PaymentAcquirerPaystack(models.Model):
         return response
 
     def call_paystack_transaction_refund(self, data):
-        
+
         endpoint = self.paystack_transaction_refund.rstrip("/")
-        
-        bearer_token = "Bearer "+str(self.paystack_merchant_key)
+
+        bearer_token = "Bearer " + str(self.paystack_merchant_key)
         headers = {"Authorization": bearer_token}
 
         api_call = requests.post(endpoint, data=data, headers=headers)
-        if api_call.status_code==200:
+        if api_call.status_code == 200:
             response = api_call.json()
         else:
             try:
@@ -125,19 +136,20 @@ class PaymentAcquirerPaystack(models.Model):
                 }
 
         return response
-    
+
     def paystack_get_form_action_url(self):
         if request.session.get("sale_order_id"):
-            sale_order = self.env["sale.order"].sudo().browse(int(request.session.get("sale_order_id")))
+            sale_order = self.env["sale.order"].sudo().browse(
+                int(request.session.get("sale_order_id")))
 
             if sale_order:
                 email = sale_order.partner_id.email
                 amount = sale_order.amount_total
-                base_url=self.env ['ir.config_parameter'].sudo ().get_param ('web.base.url')
-                callback_url = base_url.rstrip("/")+"/paystack/response"
+                base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+                callback_url = base_url.rstrip("/") + "/paystack/response"
                 data = {
                     'email': sale_order.partner_id.email,
-                    'amount': round(sale_order.amount_total, 2)*100,
+                    'amount': round(sale_order.amount_total, 2) * 100,
                     'currency': sale_order.currency_id.name,
                     'callback_url': callback_url,
                 }
@@ -148,12 +160,16 @@ class PaymentAcquirerPaystack(models.Model):
                     authorization_credential = initialize_authorization.get("data")
 
                     if authorization_credential:
-                        transaction = self.env["payment.transaction"].sudo().browse(int(request.session.get("__website_sale_last_tx_id")))
+                        transaction = self.env["payment.transaction"].sudo().browse(
+                            int(request.session.get("__website_sale_last_tx_id")))
                         if transaction:
-                            transaction.paystack_authorization_url = authorization_credential.get("authorization_url")
-                            transaction.paystack_access_code = authorization_credential.get("access_code")
-                            transaction.acquirer_reference = authorization_credential.get("reference")
-                        
+                            transaction.paystack_authorization_url = authorization_credential.get(
+                                "authorization_url")
+                            transaction.paystack_access_code = authorization_credential.get(
+                                "access_code")
+                            transaction.acquirer_reference = authorization_credential.get(
+                                "reference")
+
                     return authorization_credential.get("authorization_url")
                 else:
                     return False
@@ -161,12 +177,12 @@ class PaymentAcquirerPaystack(models.Model):
                 return False
         else:
             return False
-        
+
 
 class PaystackPaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
-    provider = fields.Selection(string="Provider", related="acquirer_id.provider")
+    provider = fields.Selection(string="Provider", related="provider_id.provider")
 
     paystack_authorization_url = fields.Char(string="Paystack Authorization")
     paystack_access_code = fields.Char(string="Paystack Access Code")
